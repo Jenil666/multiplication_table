18cd6b47f702a76beaddad94de16765198947c60
Jenil666 <jenilchandarana33@gmail.com>
2023-03-27 17:48:05 +0530

    5:47 done

---
+++ lib/userInput.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:multiplication_table/Modalclass.dart';
+
+class UserInput extends StatefulWidget {
+  const UserInput({Key? key}) : super(key: key);
+
+  @override
+  State<UserInput> createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  TextEditingController txttabledata = TextEditingController();
+  GlobalKey<FormState> check = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Form(
+          key: check,
+          child: Scaffold(
+            backgroundColor: Colors.red.shade100,
+      appBar: AppBar(
+        title: Text("Data"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter data"
+                ),
+                keyboardType: TextInputType.phone,
+                controller: txttabledata,
+                validator: (value) {
+                  if (value!.isEmpty)
+                  {
+                    return "Enter ValidData!!!!!";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(onPressed: () {
+              if(check.currentState!.validate())
+                {
+                  Navigator.pushNamed(context, 'output',arguments: txttabledata.text);
+                  txttabledata.clear();
+                }
+            },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+              child: Text("Done",),),
+          ],
+      ),
+    ),
+        ));
+  }
+}
